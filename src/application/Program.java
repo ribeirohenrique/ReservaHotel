c71bdc432f140e08c18e49709259dfdd8b244518
Henrique Mendes <henrique.r.mendes@accenture.com>
2022-05-04 19:37:38 -0300

    Solução muito ruim

---
+++ src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = scanner.nextInt();
+        System.out.print("Check-in date (DD/MM/AAAA): ");
+        Date checkIn = sdf.parse(scanner.next());
+        System.out.print("Check-out date (DD/MM/AAAA): ");
+        Date checkOut = sdf.parse(scanner.next());
+
+
+        //essa função da classe Date faz uma verificação
+        //se a data de check-out não é antes da data de check-in
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println(reservation);
+            System.out.println();
+
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Check-in date (DD/MM/AAAA): ");
+            checkIn = sdf.parse(scanner.next());
+            System.out.print("Check-out date (DD/MM/AAAA): ");
+            checkOut = sdf.parse(scanner.next());
+
+            Date now = new Date();
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("Error in reservation: Reservation dates for update must be future dates");
+            } else if (!checkOut.after(checkIn)) {
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            } else {
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
+
+            //reservation.updateDates(checkIn, checkOut);
+            //System.out.println(reservation);
+
+        }
+
+
+    }
+}
---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+
+        //Aqui foi usado o metodo da classe Date para obter
+        //A diferença em milisegundos da data de checkout para a data de checkin
+        long diff = checkOut.getTime() - checkIn.getTime();
+
+        //Foi utilizado um método da classe TimeUnit para converter
+        //O valor diff em milisegundos para dias
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Reservation: Room " + roomNumber + ", ");
+        sb.append("Check-in: " + sdf.format(checkIn) + ", ");
+        sb.append("Check-out: " + sdf.format(checkOut) + ", ");
+        sb.append(duration() + " nights");
+        return sb.toString();
+    }
+}
